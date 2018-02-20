226a8b65c24ab00f4d7a8f65e7cbe2c3dce25c31
Chintan Shah <chintanshah35@gmail.com>
2018-02-19 19:08:06 -0800

    Added YTSearch

--- src/index.js
+++ src/index.js
@@ -3,21 +3,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
 import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list';
 
 
 const API_KEY= 'AIzaSyCmOcbtpV2BCASgf1Z8mIHJxU7pySMqKxM';
 
-YTSearch({key: API_KEY, term: 'surfboards'}, function(data){
-    console.log(data);
-});
+class App extends React.Component {
+    constructor(props){
+        super(props);
 
-//This component should produce some html
-const App = ()=>{
+        this.state = { videos:[] };
+
+        YTSearch({key: API_KEY, term: 'surfboards'}, function(videos){
+            this.setState({videos: videos});//can be also written as this.setState({ videos })
+        });
+    }
+    render(){
     return (<div>
                 <SearchBar />
+                <VideoList videos={this.state.videos} />
             </div>
     );
+    }
 }
 
-//Take this component's HTML and put it on the page (in the DOM)
 ReactDOM.render(<App/>, document.querySelector('.container'));
